31a1e26b33ae86bbe5b21487977e59f72ad69253
pKrime <palucam@gmail.com>
2022-02-28 16:21:52 +0100

    version bump

--- __init__.py
+++ __init__.py
@@ -18,7 +18,7 @@
 
 bl_info = {
     "name": "Expy Kit",
-    "version": (0, 2, 2),
+    "version": (0, 4, 0),
     "author": "Paolo Acampora",
     "blender": (2, 90, 0),
     "description": "Tools for Character Rig Conversion",
