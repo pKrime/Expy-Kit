7e99d31afd81701906535693be316ae768821468
pKrime <palucam@gmail.com>
2023-10-22 14:24:19 +0200

    .

--- __refresh__.py
+++ __refresh__.py
@@ -1,6 +1,8 @@
 import os
 from importlib import reload
 from . import *
+from . import bone_utils
+# from . import _extra_
 
 def _reload_modules():
     reload(operators)
@@ -9,6 +11,7 @@ def _reload_modules():
     reload(preset_handler)
     reload(properties)
     reload(ui)
+    # reload(_extra_)
 
 _DEV_MODE = bool(os.environ.get('BLENDER_DEV_MODE', 0))
 reload_modules = _reload_modules if _DEV_MODE else lambda: None
